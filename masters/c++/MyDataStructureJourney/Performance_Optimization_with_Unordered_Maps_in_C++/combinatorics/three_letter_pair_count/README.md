a5bbebef253deb6092e0eeb8de775b889cb5d30c
jerry3008 <gbemuduj@gmail.com>
2025-07-03 15:15:58 +0100

    Add frequency-based solution to count 3-letter combinations with two identical characters

---
+++ masters/c++/MyDataStructureJourney/Performance_Optimization_with_Unordered_Maps_in_C++/combinatorics/three_letter_pair_count/README.md
@@ -0,0 +1,14 @@
+# Count 3-Letter Strings with Two Identical Characters
+
+## Problem Statement
+
+Given a string `s` of lowercase letters, count how many **3-letter combinations** can be formed from `s` such that:
+- Exactly **two characters are the same**
+- One is different
+
+---
+
+## Function Signature
+
+```cpp
+int solution(const std::string& s);
---
+++ masters/c++/MyDataStructureJourney/Performance_Optimization_with_Unordered_Maps_in_C++/combinatorics/three_letter_pair_count/main.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+long long solution(const std::string& s) 
+{
+    unordered_map<char, int>freq;
+    long long totalcount = 0;
+    
+    for(const auto & count : s)
+    {
+        freq[count]++;
+    }
+    
+    for(auto it1 = freq.begin(); it1 != freq.end(); ++it1)
+    {
+        for(auto it2 = std::next(it1); it2 != freq.end(); ++it2)
+        {
+            it1 -> second;
+            it2 -> second;
+            if(it1 -> second >=2 )
+            {
+                totalcount  += 1LL * it1 -> second * (it1 -> second - 1 ) / 2 * it2 -> second;
+            }
+            if(it2 -> second >= 2)
+            {
+                totalcount += 1LL * it2 -> second * (it2 ->second -1)/2 * it1 -> second;
+            }
+        }
+    }
+    
+    
+    
+    // TODO: implement the function
+    return totalcount;
+}
